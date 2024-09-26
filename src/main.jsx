ec16412a6af8beb046b5e25e097455cf9752275a
Kelvin Souza <kelvinsouza2017@hotmail.com>
2024-09-26 19:39:41 -0300

    :bricks: ci: Infraestrutura de rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvinder } from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Login from './routes/Login.jsx'
+import Register from './routes/Register.jsx'
+import Authentication from './routes/Authentication.jsx'
+import Orders from './routes/Orders.jsx'
+import NewOrder from './routes/NewOrder.jsx'
+
+const router = createBrowserRouter([
+  {
+    // Rota principal / Mãe
+    path:'/', element:<App />,
+    // Rota de erro
+    errorElement:<Error />,
+
+    // Rotas filhas
+    children:[
+      {path:'/Login', element:<Login />},
+      {path:'/Register', element:<Register />},
+      {path:'/Authentication', element:<Authentication />},
+      {path:'/Orders', element:<Orders />},
+      {path:'/NewOrder', element:<NewOrder />},
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvinder router={router} />
   </StrictMode>,
 )
