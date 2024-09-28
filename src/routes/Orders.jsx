73902a47dc3b63b6165623ab47a0be1ee04c4ba3
Kelvin Souza <kelvinsouza2017@hotmail.com>
2024-09-28 11:45:56 -0300

    :sparkles: Feat: Botão de logout

--- src/routes/Orders.jsx
+++ src/routes/Orders.jsx
@@ -1,12 +1,14 @@
 // import {} from 'react'
-import AppTitle from '../components/AppTitle';
 import TemporaryLinks from '../components/TemporaryLinks';
+import LogoutButton from '../components/LogoutButton';
+import AppTitle from '../components/AppTitle';
 
 function Orders() {
   return (
     <>
       <TemporaryLinks />
       {/* <h1>Pedidos</h1> */}
+      <LogoutButton />
       <AppTitle />
     </>
   )
