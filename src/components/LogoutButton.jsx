beb736bad7fd6e589a0bed8ea7f2008761df3223
Kelvin Souza <kelvinsouza2017@hotmail.com>
2024-09-28 11:40:21 -0300

    :sparkles: Feat: Botão de logout

---
+++ src/components/LogoutButton.jsx
@@ -0,0 +1,21 @@
+// // import {  } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+// function LogoutButton() {
+//   const navigate = useNavigate();
+
+//   const handleLogout = () => {
+//     // Remover token do localStorage
+//     localStorage.removeItem('authToken');
+
+//     // Redirecionar para a página de login
+//     navigate('/Login');
+//   }
+
+//   return (
+//     <>
+    
+//     </>
+//   )
+// }
+// export default LogoutButton;
