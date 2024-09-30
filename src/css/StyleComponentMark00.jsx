9108513aeeecedf79805c39759269d74309ea2d7
Kelvin Souza <kelvinsouza2017@hotmail.com>
2024-09-30 17:55:41 -0300

    :lipstick: Feat: Estilização da página inicial

--- src/css/StyleComponentMark00.jsx
+++ src/css/StyleComponentMark00.jsx
@@ -8,18 +8,21 @@ div h1 {
   font-size: 2.5rem;
   font-weight: 700;
   margin: 20px auto 10px;
-}
-
-div h2 {
-  font-size: 1.5rem;
-  font-weight: 500;
-  margin: 0 auto 30px;
-}
-
-div p {
-  font-size: 2rem;
-  font-weight: 500;
-  margin: 15px;
+  color: #5936A4;
+  }
+  
+  div h2 {
+    font-size: 1.5rem;
+    font-weight: 500;
+    margin: 0 auto 30px;
+    color: #383434;
+    }
+    
+    div p {
+      font-size: 2rem;
+      font-weight: 500;
+      margin: 15px;
+      color: #4c2e6d;
 }
 
 `
