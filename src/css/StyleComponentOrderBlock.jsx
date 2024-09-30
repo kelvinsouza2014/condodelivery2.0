032050db71d8ac33b50155deb86aeb240369120f
Kelvin Souza <kelvinsouza2017@hotmail.com>
2024-09-30 13:45:28 -0300

    :lipstick: Feat: Estilização da interface de pedidos

---
+++ src/css/StyleComponentOrderBlock.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+
+
+export const StyleComponentOrderBlock = styled.section `
+
+section .box-order-block {
+  display: flex;
+  justify-content: center;
+}
+
+section .box-order-block table {
+  width: 900px;
+  background-color: #8559E4;
+  border: 1px solid #ccc;
+}
+
+section th {
+  font-weight: 700;
+  color: #fff;
+  background-color: #8559E4;
+  height: 40px;
+  padding: 20px;
+  }
+  
+  section td {
+    height: 40px;
+    padding: 20px;
+    background-color: #fff;
+    border-left: 1px solid #d6d6d6;
+    border-bottom: 1px solid #d6d6d6;
+  }
+  
+  section .order-block-icons {
+    color: #5936A4;
+    margin: 5px;
+  }
+  
+section thead, tbody {
+
+  }
+  
+  // background-color: red;
+`
