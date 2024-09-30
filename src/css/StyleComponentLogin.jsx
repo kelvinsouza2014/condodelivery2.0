fb68022e6d01146284f9e283beadaf5476853f04
Kelvin Souza <kelvinsouza2017@hotmail.com>
2024-09-30 18:40:13 -0300

    :lipstick: Feat: Estilização da página de login

--- src/css/StyleComponentLogin.jsx
+++ src/css/StyleComponentLogin.jsx
@@ -2,8 +2,48 @@ import styled from "styled-components";
 
 
 
-export const StyleComponent = styled. `
+export const StyleComponentLogin = styled.section `
 
+section form {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
 
+section label {
+  flex-basis: 100%;
+  margin-bottom: 10px;
+}
+
+section input {
+  width: 250px;
+  height: 30px;
+  padding: 15px 10px;
+  margin-top: 5px;
+}
+
+section .submit-button-login {
+  width: 200px;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 700;
+  color: #fff;
+  background-color: #8559E4;
+  border: none;
+  border-radius: 10px;
+  margin: 10px 0 5px;
+}
+
+section .submit-button-register {
+  width: 120px;
+  height: 25px;
+  font-size: 1rem;
+  font-weight: 500;
+  color: #fff;
+  background-color: #494660bd;
+  border: none;
+  border-radius: 5px;
+  margin: 10px;
+}
 
 `
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import CryptoJS from 'crypto-js';
 import Help from '../components/Help.jsx'
 import Mark00 from '../components/Mark00.jsx';
+import { StyleComponentLogin } from '../css/StyleComponentLogin.jsx';
 
 function Login() {
 
@@ -54,26 +55,28 @@ try {
       <TemporaryLinks />
       <Help />
       <Mark00 />
+      <StyleComponentLogin>
       <section>
         <form onSubmit={handleLogin}>
           <label>
-            E-mail:
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            E-mail:<br />
+            <input placeholder="Entre com o seu e-mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </label>
           <br />
           <label>
-            Senha:
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            Senha:<br />
+            <input placeholder="Digite sua senha" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           </label>
           <br />
-          <button type="submit">Entrar</button>
+          <button className="submit-button-login" type="submit">Entrar</button>
         </form>
         <div>
-          <button onClick={() => navigate("/Register")}>
+          <button className="submit-button-register" onClick={() => navigate("/Register")}>
             Cadastre-se
           </button>
         </div>
     </section>
+    </StyleComponentLogin>
     </div>
   )
 }
